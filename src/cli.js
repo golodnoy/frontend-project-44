b36b281d94af92bd26540cd6de44756c3c743442
SVSTN <born.golod@gmail.com>
2022-09-11 12:54:12 +0300

    update files step 3

--- src/cli.js
+++ src/cli.js
@@ -1,7 +1,6 @@
 
 import readlineSync from 'readline-sync';
-
-const userName =  readlineSync.question('May I have your name?');
+console.log('Welcome to the Brain Games!');
+ export const userName =  readlineSync.question('May I have your name?');
 console.log('Hello, ' + userName + '!');
 
-export  default { userName };
