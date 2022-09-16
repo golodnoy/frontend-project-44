85c003da5959c3d7038268ea434cf1f08f10bc70
SVSTN <born.golod@gmail.com>
2022-09-17 01:12:34 +0300

    fix eslint issues

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -7,33 +7,29 @@ isScorer();
 function isScorer () {
   console.log('Answer "yes" if the number is even, otherwise answer "no".');
   let score = 0;
-  function checkAnswer(numi, answer) {
-        const correctAnswer = isEven(numi);
+  for (let i = 0; i < 100; i += 1) {
+    if (score < 3) {
+      const randomNum = readlineSync.question(`Question: ${(Math.floor(Math.random() * 100) + 1)}`);
+      const userAnswer = readlineSync.question('Your answer: ');
+      const checkAnswer = (num, answer) => {
+        const isEven = (nuv) => {
+          if (nuv % 2 === 0) {
+            return 'yes';
+          }
+          return 'no';
+        };
+        const correctAnswer = isEven(num);
         if (correctAnswer !== answer) {
-            console.log(`${answer} is wrong answer ;(. Correct answer was ${correctAnswer}.\n Let's try again, ${userName}!`);
+          console.log(`${answer} is wrong answer ;(. Correct answer was ${correctAnswer}.\n Let's try again, ${userName}!`);
         } else {
-            return true;
+          return true;
         }
-    }
-    const isEven = (num) => {
-        if (num % 2 === 0) {
-          return 'yes';
-        } else {
-        return 'no';
-    }
       };
-
-    for (let i = 0; i < 100; i += 1) {
-     if (score < 3) {
-        let randomNum = readlineSync.question('Question: '+(Math.floor(Math.random() * 100) + 1));
-        let userAnswer = readlineSync.question('Your answer: ');
-
-        const resultCheck = checkAnswer(randomNum, userAnswer);
-            if (resultCheck !== true) {
-            return resultCheck;
+      const resultCheck = checkAnswer(randomNum, userAnswer);
+      if (resultCheck !== true) {
+        return resultCheck;
         break;
       }
-
       score += 1;
       console.log(`Correct! Your score ${score}`);
     }
