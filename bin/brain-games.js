4f9612118bb7355e6f7169bb5d54d0f93bb10d3a
SVSTN <born.golod@gmail.com>
2022-10-09 22:17:51 +0300

    fix tests

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,8 +1,3 @@
 #!/usr/bin/env node
 
 import { } from '../src/index.js';
-import { } from './brain-even.js';
-import { } from './brain-calc.js';
-import { } from './brain-gcd.js';
-import { } from './brain-progression.js';
-import { } from './brain-prime.js';
