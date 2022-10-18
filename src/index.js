cd961f91aba0f9e7af313046ecc32fa7aaac1fc5
SVSTN <born.golod@gmail.com>
2022-10-18 23:52:38 +0300

    refacroing codeclimate

--- src/index.js
+++ src/index.js
@@ -7,9 +7,7 @@ function game(rules, roundfunction) {
   console.log(rules);
   let score = 0;
   for (let i = 0; i < 3; i += 1) {
-    const checkResultRound = (arr) => {
-      const correctAnswer = arr[1];
-      const userAnswer = arr[0];
+    const checkResultRound = (userAnswer, correctAnswer) => {
       const userAnswertoString = String(userAnswer);
       const userAnswertoNum = Number(userAnswer);
       let answer = Boolean;
@@ -19,18 +17,14 @@ function game(rules, roundfunction) {
         answer = (correctAnswer === userAnswertoString);
       } return answer;
     };
-    const RoundGame = () => {
-      const array = roundfunction();
-      const questionRound = array[0];
-      const correctAnswer = array[1];
-      console.log(`Question: ${questionRound}`);
-      const userAnswer = readlineSync.question('Your answer: ');
-      const resultRound = checkResultRound([userAnswer, correctAnswer]);
-      if (resultRound !== true) {
-        console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.\n Let's try again, ${userName}!`);
-      } return resultRound;
-    };
-    if (RoundGame() !== true) {
+    const questionArray = roundfunction();
+    const questionRound = questionArray[0];
+    const correctAnswer = questionArray[1];
+    console.log(`Question: ${questionRound}`);
+    const userAnswer = readlineSync.question('Your answer: ');
+    const resultRound = checkResultRound(userAnswer, correctAnswer);
+    if (resultRound !== true) {
+      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.\n Let's try again, ${userName}!`);
       break;
     } console.log('Correct!');
     score += 1;
