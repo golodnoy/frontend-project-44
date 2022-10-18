fda99cac9e97a7ab05861b99a2e942cf0d212e05
SVSTN <born.golod@gmail.com>
2022-10-19 00:29:46 +0300

    refactoring code climate

--- src/index.js
+++ src/index.js
@@ -7,30 +7,22 @@ function game(rules, roundfunction) {
   console.log(rules);
   let score = 0;
   for (let i = 0; i < 3; i += 1) {
-    const checkResultRound = (userAnswer, correctAnswer) => {
-      const userAnswertoString = String(userAnswer);
-      const userAnswertoNum = Number(userAnswer);
-      let answer = Boolean;
-      if (typeof (correctAnswer) === 'number') {
-        answer = (correctAnswer === userAnswertoNum);
-      } if (typeof (correctAnswer) === 'string') {
-        answer = (correctAnswer === userAnswertoString);
-      } return answer;
-    };
     const questionArray = roundfunction();
     const questionRound = questionArray[0];
     const correctAnswer = questionArray[1];
     console.log(`Question: ${questionRound}`);
     const userAnswer = readlineSync.question('Your answer: ');
-    const resultRound = checkResultRound(userAnswer, correctAnswer);
-    if (resultRound !== true) {
+    if (typeof (correctAnswer) === 'number' && correctAnswer !== Number(userAnswer)) {
+      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.\n Let's try again, ${userName}!`);
+      break;
+    } if (typeof (correctAnswer) === 'string' && (correctAnswer !== String(userAnswer))) {
       console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.\n Let's try again, ${userName}!`);
       break;
     } console.log('Correct!');
     score += 1;
-    if (score > 2) {
+    if (score > 2) { //
       console.log(`Congratulations, ${userName}!`);
-    }
+    }//
   }
 }
 
