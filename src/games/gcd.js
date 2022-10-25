0b4d8b1d8601af2aa20f33b7087c7231a502e00d
SVSTN <born.golod@gmail.com>
2022-10-26 00:47:08 +0300

    fix gcd

--- src/games/gcd.js
+++ src/games/gcd.js
@@ -1,23 +1,8 @@
 import runGame from '../index.js';
-import getDividers from '../dividers.js';
 import getRandomIntInclusive from '../util.js';
 
+const calcTotalDiv = (a, b) => (b === 0 ? a : calcTotalDiv(b, a % b));
 export const createQuestionGcd = () => {
-  const calcTotalDiv = (num1, num2) => {
-    function getMaxOfArray(numArray) {
-      return Math.max.apply(null, numArray);
-    }
-    const arrayNum1 = getDividers(num1);
-    const arrayNum2 = getDividers(num2);
-    const arrayComDividers = [];
-    for (let i = 0; i < arrayNum1.length; i += 1) {
-      if (arrayNum2.includes(arrayNum1[i])) {
-        arrayComDividers.push(arrayNum1[i]);
-      }
-    }
-    const maxDivider = getMaxOfArray(arrayComDividers);
-    return maxDivider;
-  };
   const firstValue = getRandomIntInclusive(1, 100);
   const secondValue = getRandomIntInclusive(1, 100);
   const question = `${firstValue} ${secondValue}`;
